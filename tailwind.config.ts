21bd91174d7bab5f2865243d79cdd15ca2a2e443
Eduardo Reche <edu.reche@gmail.com>
2023-08-19 20:41:37 -0300

    app: basic layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
       colors: {
-
-        rocket: '#8257e6'
-      }
+        rocket: '#8257e6',
+      },
     },
   },
   plugins: [],
