fccf232d3cf44c43e9313a565734b0132d550467
Eduardo Reche <edu.reche@gmail.com>
2023-08-20 21:17:10 -0300

    improve form dark mode

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -12,7 +12,7 @@ interface SelectProps extends SelectPrimitive.SelectProps {
 export function Select({ placeholder, children, ...props }: SelectProps) {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600 dark:border-zinc-700 dark:bg-zinc-700 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20 dark:data-[placeholder]:text-zinc-400">
+      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100 data-[placeholder]:text-zinc-600 dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20 dark:data-[placeholder]:text-zinc-400">
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-black"
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -39,7 +39,7 @@ export function Sidebar() {
       >
         <Input.Root>
           <Input.Prefix>
-            <Search className="h-5 w-5 to-zinc-500" />
+            <Search className="h-5 w-5 text-zinc-500 dark:text-zinc-600" />
           </Input.Prefix>
           <Input.Control placeholder="Search" />
         </Input.Root>
